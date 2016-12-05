211675327fdee763f5af1be90927d596938af4f6
meomancer <meomancer@gmail.com>
2016-12-05 14:23:52 +0700

    change sftp as extention of pg backups

--- backups.sh
+++ backups.sh
@@ -14,10 +14,35 @@ cd ${MYBACKUPDIR}
 
 echo "Backup running to $MYBACKUPDIR" >> /var/log/cron.log
 
+# ------------------------------------------------------------------------
+# BACKUP Postgree First
+# Loop through each pg database backing it up
+# ------------------------------------------------------------------------
+DBLIST=`psql -l | awk '{print $1}' | grep -v "+" | grep -v "Name" | grep -v "List" | grep -v "(" | grep -v "template" | grep -v "postgres" | grep -v "|" | grep -v ":"`
+# echo "Databases to backup: ${DBLIST}" >> /var/log/cron.log
+for DB in ${DBLIST}
+do
+  echo "Backing up $DB"  >> /var/log/cron.log
+  FILENAME=${MYBACKUPDIR}/${DUMPPREFIX}_${DB}.${MYDATE}.dmp
+  pg_dump -i -Fc -f ${FILENAME} -x -O ${DB}
+done
 
-echo "Backing up $TARGET_FOLDER"  >> /var/log/cron.log
+FILENAME=${MYBACKUPDIR}/[GLOBAL]${DUMPPREFIX}.${MYDATE}.sql
+pg_dumpall -i -f ${FILENAME} -x -O --globals-only
+
+
+# ------------------------------------------------------------------------
+# Preparing for sftp backups
+# ------------------------------------------------------------------------
 FILENAME=${MYBACKUPDIR}/${DUMPPREFIX}.${MYDATE}.tar.gz
-tar -zcvf ${FILENAME} ${TARGET_FOLDER}/*
+echo "Creating compressed file $FILENAME"  >> /var/log/cron.log
+
+cd ${MYBACKUPDIR}
+FILES=`ls -I "*.tar.gz" | grep ${MYDATE}`
+tar -zcvf ${FILENAME} ${FILES}
+
+# ------------------------------------------------------------------------
+# push to remote sever
+# ------------------------------------------------------------------------
 echo "push backup to remote server" >> /var/log/cron.log
 /usr/bin/python /sftp_remote.py ${FILENAME} >> /var/log/cron.log
-
